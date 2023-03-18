09ebfb357d925709dbd8594db70b2007b0fbbbb3
Zolice <vividzhenlong@gmail.com>
2023-03-18 17:49:39 +0800

    Fixed /clearcontext crash
    Crash related to registering a channel twice.

--- commands/clearcontext.js
+++ commands/clearcontext.js
@@ -7,21 +7,24 @@ module.exports = {
         .setDescription('Clear the context for you/this channel.'),
     async execute(interaction, client) {
         console.log("Clear Context command executed by " + interaction.user.username + " in " + interaction.channel.name)
+        let response = "This channel has not been registered yet."
 
         if (client.config.discord.registeredServers[interaction.guildId]) {
             client.config.discord.registeredServers[interaction.guildId].openAiChannelIDs.general.forEach(channelId => {
                 if (channelId == interaction.channelId) {
                     client.context.generalChat[interaction.user.id] = []
-                    interaction.reply({ content: 'Context has been cleared for yourself. You can now start a new conversation.', ephemeral: true });
+                    response = { content: 'Context has been cleared for yourself. You can now start a new conversation.', ephemeral: true }
                 }
             })
             client.config.discord.registeredServers[interaction.guildId].openAiChannelIDs.multiplayer.forEach(channelId => {
                 if (channelId == interaction.channelId) {
                     client.context.multiplayerChat[interaction.channelId] = []
-                    interaction.reply('Context has been cleared for this channel. You can now start a new conversation.');
+                    response = 'Context has been cleared for this channel. You can now start a new conversation.'
                 }
             })
         }
+
+        interaction.reply(response)
 
         fs.writeFileSync(`./context.json`, JSON.stringify(client.context, null, 4), err => {
             if (err) console.log(err)
