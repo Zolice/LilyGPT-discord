c27e9cbb6d8ab1d95fa2c68587ebe10318491362
Zolice <vividzhenlong@gmail.com>
2023-04-14 19:26:59 +0800

    Added ClearContext command

--- commands/commandManager.ts
+++ commands/commandManager.ts
@@ -1,8 +1,10 @@
 import { Collection } from 'discord.js';
 import { sayData, sayExecute } from './say'
+import { clearData, clearExecute } from './clearcontext';
 
 export const Commands = new Collection<string, any>()
 
 export const CommandManager = () => {
     Commands.set(sayData.name, sayExecute)
+    Commands.set(clearData.name, clearExecute)
 }
--- commands/say.ts
+++ commands/say.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import CheckAdmin from '../functions/checkAdmin'
+import { CommandInteraction, Client } from 'discord.js'
 
 export const sayData = new SlashCommandBuilder()
     .setName('say')
@@ -10,11 +11,11 @@ export const sayData = new SlashCommandBuilder()
             .setRequired(true)
     )
 
-export const sayExecute = async (interaction, client) => {
-    console.log("Say command executed: " + interaction.options.getString('message') + " by " + interaction.user.username + " in " + interaction.channel.name)
+export const sayExecute = async (interaction: CommandInteraction, client: Client) => {
+    console.log("Say command executed: " + interaction.options.get('message').value + " by " + interaction.user.username + " in " + interaction.channel.name)
 
     if (!CheckAdmin(interaction.guildId, interaction.user.id, client)) return interaction.reply({ content: 'Error: Administrative rights required.', ephemeral: true })
 
-    interaction.channel.send(interaction.options.getString('message'))
+    interaction.channel.send(interaction.options.get('message').value as string)
     await interaction.reply({ content: 'Sent!', ephemeral: true })
 }
