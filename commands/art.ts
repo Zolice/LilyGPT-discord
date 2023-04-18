e1bb21e31f15d4c72b86e0aa3a4b182b5d8b890e
Yong Zhen Long <vividzhenlong@gmail.com>
2023-04-18 14:00:01 +0800

    Added Art command

---
+++ commands/art.ts
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction, Client } from 'discord.js'
+import GenerateArt from '../gpt/generateArt'
+import ConversationSingle from '../gpt/conversationSingle'
+
+export const sayData = new SlashCommandBuilder()
+    .setName('art')
+    .setDescription('Generate art using Dall-E')
+    .addStringOption(option =>
+        option.setName('request')
+            .setDescription('The description of the art to generate')
+            .setRequired(true)
+    )
+
+export const sayExecute = async (interaction: CommandInteraction, client: Client) => {
+    // await interaction.reply({ content: 'Generating your Art!' })
+    let request = interaction.options.get('request').value as string
+    if (request.length <= 0) {
+        interaction.reply({ content: "Tell me what you would like to draw, with /art <search term>", ephemeral: true })
+        return
+    }
+
+    GenerateArt(request, interaction.user.username).then(async (response: string) => {
+        if (response.startsWith("Sorry")) {
+            interaction.reply(response)
+            return
+        }
+
+        let quote = await ConversationSingle(`I have drawn a ${request}, write a short quote about it.`)
+        await interaction.reply(`${quote}\n${response}`)
+    })
+
+}
